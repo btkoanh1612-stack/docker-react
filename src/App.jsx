03fe4b0c78799c750ecd65a3b9cc6d69697756ce
btkoanh-stack <btkoanh1612@gmail.com>
2025-11-24 23:21:12 +0700

    Final: Add App.jsx to resolve test import error

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// File: src/App.js (hoặc App.jsx)
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        {/* Đây là văn bản mà file App.test.js đang tìm kiếm */}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
