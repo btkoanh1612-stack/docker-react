a7f8a8fd1afa0b0d7284a97166811e5640920c39
btkoanh-stack <btkoanh1612@gmail.com>
2025-11-25 09:48:28 +0700

    Fix: Import jest-dom to enable toBeInTheDocument

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-// File: src/App.test.js
+import '@testing-library/jest-dom';
 
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// Đây là test đơn giản mà react-scripts dùng để kiểm tra App.js
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
